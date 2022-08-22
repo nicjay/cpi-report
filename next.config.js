3e5517401708550e2f99317777c19c5c20ffab3e
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2022-08-22 07:07:11 -0700

    Add Tailwind; Configure options

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  experimental: {
+    images: {
+      allowFutureImage: true
+    },
+    newNextLinkBehavior: true,
+    browsersListForSwc: true,
+    legacyBrowsers: false
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
