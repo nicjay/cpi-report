29846c980e8616b25e97fbaed7da2e8a279835a9
Nick Jordan <5668079+nicjay@users.noreply.github.com>
2023-03-20 14:17:04 -0700

    Migrate to /app dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {}
